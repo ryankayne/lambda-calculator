f7fd164dad9b79379fe06c34796e4172fbc20bb3
ryankayne <coope1rk@gmail.com>
2019-11-05 17:54:00 -0500

    imports and exports

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const NumberButton = () => {
+
+const NumberButton = props => {
+  console.log(props);
+  console.log();
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,5 +1,6 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton";
 //import any components needed
 
 //Import your array data to from the provided data file
